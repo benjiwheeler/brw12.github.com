ec7f9642a1e02371fddb40dd10a79471a6919768
Ben Wheeler <wheeler.benjamin@gmail.com>
2013-04-02 16:40:40 -0400

    added jquery library and functionality

--- jscourse/main.js
+++ jscourse/main.js
@@ -1,1 +1,22 @@
+$(document).ready(function(){
+    
+    function productlinkover() {
+        $(this).addClass("productlinkhover");
+        $(this).find(".productdesc").hide();
+        $(this).find(".morebutton").show();
+        var productname = $(this).attr("id");
+        var titleid = "#" + productname + "title";
+        $("div").find(".title").hide();
+        $("div").find(titleid).show();
+        }
+    function productlinkout(){
+        $(this).removeClass("productlinkhover");
+        $(this).find(".productdesc").show();
+        $(this).find(".morebutton").hide();
+        $("div").find(".title").hide();
+        $("div").find("#defaulttitle").show();
+    }
+
+        $("#productlist li").on("mouseover", productlinkover).on("mouseout", productlinkout);
+});
 
